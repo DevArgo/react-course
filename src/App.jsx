94b8b2fa8f8242a8a15b208cfc863286807ed361
Massimo Lauriola <lauriola@argomedia.it>
2024-09-30 19:11:26 +0200

    hoocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
+const useCounter = () => {
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => setCounter(() => counter + 1);
+
+    const reset = () => setCounter(0);
+
+    const decrement = () => setCounter(() => counter - 1);
+
+    return [counter, reset, increment, decrement];
+}
+
 const App = ({ name, color = null, onNameClick = () => { } }) => {
+    const [counter, , increment] = useCounter();
     return <>
-        <h1>Benvenuti in ReactJs!</h1>
+        <h1 onClick={increment}>Benvenuti in ReactJs!</h1>
         <h2>Ciao <span onClick={onNameClick} style={
             {
                 color: color ?? "red"
             }
         }>{name}</span>, come va?</h2>
+        <h3>{counter}</h3>
     </>;
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,38 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import Main, {PrimaryButton} from './App.jsx'
+import Main, { PrimaryButton } from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Main
-            name="Max"
-            color="yellow"
-            onNameClick={() => alert("Ciao!")}
-        />
+let timerId = null;
+
+const CountdonwApp = () => {
+
+    const [countdown, setCountdown] = useState(10);
+    const [countdown2, setCountdown2] = useState(10);
+
+    return <>
+        <div onClick={() => setCountdown(() => countdown - 1)}>
+            <div>{countdown}</div>
+            {
+                countdown > 0 ? <Main
+                    name="Max"
+                    color="yellow"
+                    onNameClick={() => alert("Ciao!")}
+                /> : null
+            }
+        </div>
+        <p onClick={() => setCountdown2(() => countdown2-1)}>{countdown2}</p>
         <Main
             name="Alessia"
             color="green"
             onNameClick={() => alert("Come va?")}
         />
-        <PrimaryButton text="Alla prossima" handleClick={() => alert("ci sentiamo!")}/>
+        <PrimaryButton text="Alla prossima" handleClick={() => alert("ci sentiamo!")} />
+
+    </>
+}
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <CountdonwApp />
     </StrictMode>,
 )
